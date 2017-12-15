5c0270e81c7ab373ee1eb2331b0810305d729172
Enas Siddiqi <enassiddiqi@Enass-MacBook-Pro.local>
2017-12-15 13:02:48 -0600

    UI and JS to move characters to respective divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,143 @@
-// var gameObj = {
+var charlie = {
+	name: "Charlie",
+	health: 120,
+	baseAttack: 10,
+	attack: 10
+}
+
+var dee = {
+	name: "Dee",
+	health: 125,
+	baseAttack: 12,
+	attack: 12
+}
+
+var dennis = {
+	name: "Dennis",
+	health: 150,
+	baseAttack: 15,
+	attack: 15
+}
+
+var frank = {
+	name: "Frank",
+	health: 110,
+	baseAttack: 10,
+	attack: 10
+}
+
+var mac = {
+	name: "Mac",
+	health: 130,
+	baseAttack: 10,
+	attack: 10
+}
 	
-// 	characterSelected: false,
+var character = {};
+var defender = {};
+var enemiesDefeated = 0;
 
-// 	defenderSelected: false,
+var characterChosen = false;
+var defenderChosen = false;
+var gameOver = false;
 
-// 	character: {},
-
-// 	defender: {},
-
-// 	// CHARACTERS
-// 	characters: [
-// 		charlie: {
-// 			name: "Charlie",
-// 			health: 120,
-// 			baseAttack: 10,
-// 			attack: 10
-// 		},
-
-// 		dee: {
-// 			name: "Dee",
-// 			health: 125,
-// 			baseAttack: 12,
-// 			attack: 12
-// 		},
-
-// 		dennis: {
-// 			name: "Dennis",
-// 			health: 150,
-// 			baseAttack: 15,
-// 			attack: 15
-// 		},
-
-// 		frank: {
-// 			name: "Frank",
-// 			health: 110,
-// 			baseAttack: 10,
-// 			attack: 10
-// 		},
-
-// 		mac: {
-// 			name: "Mac",
-// 			health: 130,
-// 			baseAttack: 10,
-// 			attack: 10
-// 		}
-// 	]
-
-// }
 
 	$("#restart").hide();
 
 // When you click on a character, your character moves to "Your Character"
 
 	$("#charlie").on("click", function() {
+		// choose as your character
+		if (!characterChosen) {
+			console.log("Charlie is the chosen character");
+			$("#charlie").removeClass("available-character").addClass("chosen-character");
+			$("#your-character").append(this);
+			character = charlie;
+			characterChosen = true;
+			enemies();
+		} else if (characterChosen && !defenderChosen) {
+			console.log("Charlie is the chosen defender");
+			$("#charlie").removeClass("enemy-character").addClass("defender-character");
+			$("#defender-section").append(this);
+			defender = charlie;
+			defenderChosen = true;
+		}
 
-		$("#charlie").removeClass("available-character").addClass("chosen-character");
-		$("#your-character").append(this);
-
-		enemies();
 	})
 
 
 	$("#dee").on("click", function() {
-
-		$("#dee").removeClass("available-character").addClass("chosen-character");
-		$("#your-character").append(this);
-
-		enemies();
+		// choose as your character
+			if (!characterChosen) {
+			console.log("Dee is the chosen character");
+			$("#dee").removeClass("available-character").addClass("chosen-character");
+			$("#your-character").append(this);
+			character = dee;
+			characterChosen = true;
+			enemies();
+		} else if (characterChosen && !defenderChosen) {
+			console.log("Dee is the chosen defender");
+			$("#dee").removeClass("enemy-character").addClass("defender-character");
+			$("#defender-section").append(this);
+			defender = dee;
+			defenderChosen = true;
+		}
 	})
 
 
 	$("#dennis").on("click", function() {
-
-		$("#dennis").removeClass("available-character").addClass("chosen-character");
-		$("#your-character").append(this);
-
-		enemies();
+		if (!characterChosen) {
+				console.log("Dennis is the chosen character");
+				$("#dennis").removeClass("available-character").addClass("chosen-character");
+				$("#your-character").append(this);
+				character = dennis;
+				characterChosen = true;
+				enemies();
+			} else if (characterChosen && !defenderChosen) {
+				console.log("Dennis is the chosen defender");
+				$("#dennis").removeClass("enemy-character").addClass("defender-character");
+				$("#defender-section").append(this);
+				defender = dennis;
+				defenderChosen = true;
+			}
 	})
 
 
 	$("#frank").on("click", function() {
 
-		$("#frank").removeClass("available-character").addClass("chosen-character");
-		$("#your-character").append(this);
-
-		enemies();
+			if (!characterChosen) {
+				console.log("Frank is the chosen character");
+				$("#frank").removeClass("available-character").addClass("chosen-character");
+				$("#your-character").append(this);
+				character = frank;
+				characterChosen = true;
+				enemies();
+			} else if (characterChosen && !defenderChosen) {
+				console.log("Frank is the chosen defender");
+				$("#frank").removeClass("enemy-character").addClass("defender-character");
+				$("#defender-section").append(this);
+				defender = frank;
+				defenderChosen = true;
+			}
 	})
 
 
 	$("#mac").on("click", function() {
 
-		$("#mac").removeClass("available-character").addClass("chosen-character");
-		$("#your-character").append(this);
-
-		enemies();
+		if (!characterChosen) {
+			console.log("Mac is the chosen character");
+			$("#mac").removeClass("available-character").addClass("chosen-character");
+			$("#your-character").append(this);
+			character = mac;
+			characterChosen = true;
+			enemies();
+		} else if (characterChosen && !defenderChosen) {
+			console.log("Mac is the chosen defender");
+			$("#mac").removeClass("enemy-character").addClass("defender-character");
+			$("#defender-section").append(this);
+			defender = mac;
+			defenderChosen = true;
+		}
 	})
 
 
@@ -111,10 +155,16 @@ var enemies = function () {
 // 	(Background changes to black, border to green)
 
 
+
 // When you click on attack button, a message appears:
 // 	"You attacked *DEFENDER* for *#* damage. \n  *DEFENDER* attacked you back for *##* damage."
 // and your character and defender's health decrease the respective value.
+var attack = function () {
+	$("#attack").on("click", function(){
+		$("#game-message").html("You attacked " + defender.name + " for " + character.attack + ".");
 
+	})
+}
 
 // Everytime attack button is clicked, your character's attack doubles.
 
